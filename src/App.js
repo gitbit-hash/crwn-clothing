a26e03374e30cd161e6e454cd128caa913d4f040
unknown <codeprof2016@hotmail.com>
2019-12-20 00:26:19 +0200

    updating the app with a ShopPage, ColectionPreview and CollectionItem component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Route,Switch } from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component'
-import './App.css';
+import { Route,Switch } from 'react-router-dom';
 
-const Hats = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import './App.css';
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={Hats} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 }
