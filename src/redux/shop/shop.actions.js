61ca0616f367e27981facc19863831b9990b955e
unknown <codeprof2016@hotmail.com>
2020-01-04 14:08:38 +0200

    updating our shop component to use thunk middleware

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsSnapshotTomap } from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTION_START
+});
+
+export const fetchCollectionSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTION_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotTomap(snapshot);
+        dispatch(fetchCollectionSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  }
+}
